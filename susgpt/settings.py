7c276b6a837be8958e399ff23c3e62eb6425368a
YashKandoi <yashkandoi2003@gmail.com>
2024-06-17 12:42:06 +0530

    shifted database to postgres

--- susgpt/settings.py
+++ susgpt/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'susgpt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'susgpt',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'PORT': '5432',
+        'HOST': 'localhost',
     }
 }
 
