1cfe5f2c9702ab6b3c4ddce8d758dd4467e1cee2
YashKandoi <yashkandoi2003@gmail.com>
2024-06-17 14:44:36 +0530

    aaaa

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 # Build the project
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
-python3.9 -m vercel dev
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
